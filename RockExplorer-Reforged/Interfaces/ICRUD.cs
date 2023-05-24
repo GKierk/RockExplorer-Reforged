6f26f3a13839d59139b7d4f1bb31af3fc2a107da
Gabriel H. Kierkegaard <gabriel_kierkegaard@protonmail.com>
2023-05-24 09:35:31 +0200

    Add project files.

---
+++ RockExplorer-Reforged/Interfaces/ICRUD.cs
@@ -0,0 +1,15 @@
+﻿namespace RockExplorer_Reforged.Interfaces
+{
+    public interface ICRUD<T>
+    {
+        public void Create (T entity);
+
+        public T Read(int key);
+
+        public Dictionary<int, T> ReadAll();
+
+        public void Update(int key, T entity);
+
+        public void Delete(int key);
+    }
+}
