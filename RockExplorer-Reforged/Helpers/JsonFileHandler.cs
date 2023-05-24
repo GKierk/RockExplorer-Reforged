e28cf6247ab9ec92a46c21845069c6cb0009a633
Gabriel H. Kierkegaard <gabriel_kierkegaard@protonmail.com>
2023-05-24 14:07:12 +0200

    Reimplemented JsonFileHandler.

---
+++ RockExplorer-Reforged/Helpers/JsonFileHandler.cs
@@ -0,0 +1,83 @@
+﻿using RockExplorer_Reforged.Models;
+using System.Text.Json;
+
+namespace RockExplorer_Reforged.Helpers
+{
+    public class JsonFileHandler
+    {
+        public static Dictionary<int, Artifact> ReadJson(string path)
+        {
+            if (!File.Exists(path))
+            {
+                Default(path);
+            }
+
+            using (var jsonFileReader = File.OpenText(path))
+            {
+                var json = jsonFileReader.ReadToEnd();
+                return JsonSerializer.Deserialize<Dictionary<int, Artifact>>(json);
+            }
+        }
+
+        public static void WriteToJson(string path)
+        {
+            ArtifactRepository repo = ArtifactRepository.Instance;
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+
+                using (FileStream outputStream = File.OpenWrite(path))
+                {
+                    var writer = new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = false,
+                        Indented = true
+                    });
+
+                    JsonSerializer.Serialize<Dictionary<int, Artifact>>(writer, repo.Artifacts);
+                }
+            }
+            else
+            {
+
+                using (FileStream outputStream = File.OpenWrite(path))
+                {
+                    var writer = new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = false,
+                        Indented = true
+                    });
+
+                    JsonSerializer.Serialize<Dictionary<int, Artifact>>(writer, repo.Artifacts);
+                }
+            }
+        }
+
+        private static void Default(string path)
+        {
+            Dictionary<int, Artifact> defaultValues = new Dictionary<int, Artifact>();
+
+            defaultValues[1] = new Artifact
+            {
+                Name = "placeholder",
+                Description = "placeholder",
+                YearOfCreation = 2023,
+                Artist = "placeholder",
+                PathToAudioFile = "",
+                PathToImageFile = ""
+            };
+
+            using (FileStream outputStream = File.OpenWrite(path))
+            {
+                var writer = new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                {
+                    SkipValidation = false,
+                    Indented = true
+                });
+
+                JsonSerializer.Serialize<Dictionary<int, Artifact>>(writer, defaultValues);
+            }
+        }
+    }
+}
