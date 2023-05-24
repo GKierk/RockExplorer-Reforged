4842f4c41a16e0dd28c31776e649a9f94c5db9d2
Gabriel H. Kierkegaard <gabriel_kierkegaard@protonmail.com>
2023-05-24 09:41:20 +0200

    Reimplemented Artifact.

---
+++ RockExplorer-Reforged/Models/Artifact.cs
@@ -0,0 +1,18 @@
+﻿namespace RockExplorer_Reforged.Models
+{
+    public class Artifact
+    {
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public int YearOfCreation { get; set; }
+
+        public string? Artist { get; set; }
+
+        public string? PathToAudioFile { get; set; }
+
+        public string? PathToImageFile { get; set; }
+
+    }
+}
