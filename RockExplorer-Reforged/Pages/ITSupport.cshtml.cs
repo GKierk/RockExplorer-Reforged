da6c2cccfa163faceeeb930235fb00ee7d7b42fa
Gülsüm Erdogan <guls0489@edu.zealand.dk>
2023-05-25 11:06:35 +0200

    Made an IT Support page.

---
+++ RockExplorer-Reforged/Pages/ITSupport.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RockExplorer_Reforged.Pages
+{
+    public class ITSupportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
