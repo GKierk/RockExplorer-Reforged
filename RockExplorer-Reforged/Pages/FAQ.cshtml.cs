18d2fc175c5bf36e48961a6bf25086eb4b2b0ad9
Gülsüm Erdogan <guls0489@edu.zealand.dk>
2023-05-25 11:06:15 +0200

    Made a FAQ page.

---
+++ RockExplorer-Reforged/Pages/FAQ.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RockExplorer_Reforged.Pages
+{
+    public class FAQModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
